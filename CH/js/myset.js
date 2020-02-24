36ac340ac100f6f3ec76478161c0873da5f37712
Lize Wu <retniw72@gmail.com>
2020-02-24 14:54:22 +0800

    2020-0224-第一次上傳

---
+++ CH/js/myset.js
@@ -0,0 +1,182 @@
+$(function(){
+
+	// 第二節點靠齊高度
+	// 把 a.dropdown-toggle 的高度數值，丟進.dropdown-menu 的top
+	var _first = $('#Header .Left .nav>li a');
+		_value = _first.outerHeight();
+	// alert(_value);
+
+	// $('.dropdown-menu').css({
+	// 	top: _value
+	// });
+
+	// 左右主選單 點一個秀一個
+	$('#Header .navbar-header .pull-right').click(function(){
+		$(this).parent().siblings().eq(1).removeClass('in');
+	});
+	$('#Header .navbar-header .pull-left').click(function(){
+		$(this).parent().siblings().eq(0).removeClass('in');
+	});
+
+
+	// 錨點滑動
+	$(function(){
+		// 輔導業務
+		$("a[href=#Busniess]").click(function() {
+			$("html,body").animate({
+				scrollTop:$("#Busniess").offset().top
+			}, 600);
+		});
+		// 新聞稿
+		$("a[href=#News]").click(function() {
+			$("html,body").animate({
+				scrollTop:$("#News").offset().top
+			}, 600);
+		});
+		// 業務消息
+		$("a[href=#BizNews]").click(function() {
+			$("html,body").animate({
+				scrollTop:$("#BizNews").offset().top
+			}, 1000);
+		});
+		// 活動訊息
+		$("a[href=#ActInfo]").click(function() {
+			$("html,body").animate({
+				scrollTop:$("#ActInfo").offset().top
+			}, 1000);
+		});
+		// 粉絲專頁
+		$("a[href=#Community]").click(function() {
+			$("html,body").animate({
+				scrollTop:$("#Community").offset().top
+			}, 1600);
+		});
+	})
+
+	// Go Top
+	$(function(){
+		var GoTop_Offset = $('#Btn_GoTop').offset().top;
+
+		$('#Btn_GoTop').hide();
+
+		$(window).scroll(function() {
+			if ( $(this).scrollTop() > 300){
+				$('#Btn_GoTop').fadeIn(600);
+			}else {
+				$('#Btn_GoTop').stop().fadeOut(600);
+			}
+		});
+		$('#Btn_GoTop').click(function(){
+			$('html,body').animate({scrollTop:0},1200);
+			return false;
+		});
+	});
+
+	// Animate
+	$(function(){
+		$('').hover(function(){},function(){})
+	})
+
+	//Fatfooter 內容收合
+	$(function(){
+		$(".FatFooterBtn").click(function() {
+			$('#FatFooter>div>nav>ul>li>ul').slideToggle(function() {
+				if ($(this).is(':visible')) {
+					document.getElementById("FatFooterBtn").value = "收合";
+				} else {
+					document.getElementById("FatFooterBtn").value = "展開";
+				}
+			});
+			$(this).toggleClass('close');
+		});
+	});
+
+	// 字級調整
+	$(function(){
+		$(".FontSmall").click(function(){
+			$(this).siblings().removeClass('active');
+			$(this).addClass('active');
+			$(".Content_page").css({fontSize:"85%"});
+		});
+		$(".FontDefault").click(function(){
+			$(this).siblings().removeClass('active');
+			$(this).addClass('active');
+			$(".Content_page").css({fontSize:"100%"});
+	  	});
+		$(".FontLarge").click(function(){
+			$(this).siblings().removeClass('active');
+			$(this).addClass('active');
+			$(".Content_page").css({fontSize:"115%"});
+		});
+	});
+
+	// .According 展開收合
+	$('.btn-switch').click(function(event) {
+		/* Act on the event */
+		// $(this).parents('.btn_grp').siblings('.box').addClass('act');
+		var _this = $(this).parent('').siblings('.box');
+
+		// 如果沒有.act 被點擊時，加入.act
+		// 如果有.act 被點擊時，移除.act
+		if ( _this.hasClass('act')) {
+			_this.removeClass('act');
+			$(this).val('展開內容');
+		} else {
+			_this.addClass('act');
+			$(this).val('收合內容');
+		}
+	}); 
+
+
+
+	// U00頁籤 eason+偉良
+	// jQuery(document).ready(function() {
+	//     $('.tabs').find('.active').next('.tabContent').fadeIn(); //顯示選到的頁籤的內容。用css設定也可以，本範例中css也有設定（18、19行）
+	//     // var tw = $('.tabSet').width(); //取得頁籤區塊的寬度，並存入變數tw中
+	//     var tabItemHeight = $('.tabs>h1>a').innerHeight(); //取得頁籤項目含padding高度，並存入變數tabItemHeight中
+	//     $('.tabs').find('.tabContent').css('top', tabItemHeight); //找到頁籤內容區塊，設定它的top等於頁籤項目高度
+	//     $(window).on('load resize', function() {
+	//     	var tw = $('.tabSet').width()*0.98; 
+	//         $('.tabSet').each(function() { //各別處理每個頁籤組
+
+	//         	var twSelf = $(this).width();
+	//             var tabContentHeight = $(this).find('.active').next('.tabContent').innerHeight(); //找到被選到的頁籤內容，取得它含padding的高度，並將高度存到變數tabContentHeight中。
+	//             var tabItemLength = $(this).find('h1').length; //取得每個頁籤組中頁籤項目的個數
+
+	//             $(this).height(tabContentHeight + tabItemHeight); //頁籤組的總高度＝頁籤內容的高度＋頁籤項目的高度
+	//             $(this).find('h1>a').width(twSelf / tabItemLength); //頁籤項目的寬度＝頁籤組的寬度／頁籤項目個數 
+	//         });
+	//     });
+	//     $('.tabs>h1>a').focus(tabs); //用鍵盤選到頁籤時執行tabs函式
+	//     $('.tabs>h1>a').click(tabs); //用滑鼠選到頁籤時執行tabs函式
+
+	//     function tabs() { //定義tabs函式
+	//         $(this).parent('h1').siblings().removeClass('active'); //非選到的頁籤項目的 class 要移除
+	//         $(this).parent('h1').addClass('active'); //把選到的頁籤項目加上 class="active"
+	//         tabContentHeight = $(this).parent('h1').next('.tabContent').innerHeight(); //取得選到的頁籤內容含padding的高度
+	//         $(this).parents('.tabSet').height(tabContentHeight + tabItemHeight); //頁籤組的總高度＝頁籤內容的高度＋頁籤項目的高度
+	//     }
+	// });
+
+	// fancy box
+	// $(function(){
+	// 	/* Button helper. Disable animations, hide close button, change title type and content */
+	// 	$('.fancybox-buttons').fancybox({
+	// 		openEffect  : 'none',
+	// 		closeEffect : 'none',
+	// 		prevEffect : 'none',
+	// 		nextEffect : 'none',
+	// 		closeBtn  : true,
+	// 		helpers : {
+	// 			title : {
+	// 				type : 'inside'
+	// 			},
+	// 			buttons : {}
+	// 		},
+	// 		afterLoad : function() {
+	// 			this.title = 'Image ' + (this.index + 1) + ' of ' + this.group.length + (this.title ? ' - ' + this.title : '');
+	// 		}
+	// 	});
+	// });
+
+})
---
+++ EN/js/slick-myset.js
@@ -0,0 +1,83 @@
+$(function(){
+    // 單張輪播
+    $('.Singleplay').slick({
+        arrows: true,
+        slidesToShow: 1,         //一次顯示幾張
+        slidesToScroll: 1,       //一次卷幾張
+        dots: true,              //顯示圓點
+        dotsClass: 'slick-dots', //圓點樣式
+        // 自訂左右箭頭
+        // next arrow
+        prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
+        // next arrow
+        nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
+        infinite: true,          //無限輪播
+        autoplay: false,          //自動輪播
+        autoplaySpeed: 3000      //輪播速度
+    });
+
+    // Banner輪播
+    $('.BannerSlide').slick({
+        arrows: true,
+        slidesToShow: 1,         //一次顯示幾張
+        slidesToScroll: 1,       //一次卷幾張
+        dots: false,              //顯示圓點
+        dotsClass: 'slick-dots', //圓點樣式
+        // 自訂左右箭頭
+        // next arrow
+        prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
+        // next arrow
+        nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
+        infinite: true,          //無限輪播
+        autoplay: true,          //自動輪播
+        autoplaySpeed: 3000      //輪播速度
+    });
+
+    // 側邊欄 垂直輪播
+    $('.Vertical_slider').slick({
+        dots: false,
+        infinite: true,
+        vertical: true,
+        verticalSwiping: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        autoplay: true,
+        autoplaySpeed: 1500,
+
+        speed: 1000,
+        focusOnSelect: true,
+    });
+
+    // 
+    $('.Responsive_slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        responsive: [{
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 720,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+});
